ee1c6776a96708c2bdd132415b851134b1bcdcb8
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-26 19:31:29 -0300

    feat(index): add components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
-import TestComponent from 'components/TestComponent'
+import { fetchedData } from 'atoms/fetchedData'
+import CollectionInfo from 'components/CollectionInfo'
+import Header from 'components/Header'
+import SalesCard from 'components/SalesCard'
 import type { NextPage } from 'next'
-import * as S from '../styles/pages/index.styles'
+import { useRecoilState } from 'recoil'
+import { data } from 'utils/mock'
 
 const Home: NextPage = () => {
+  const [, setData] = useRecoilState(fetchedData)
+  setData(data)
+
   return (
-    <S.Container>
-      <TestComponent />
-    </S.Container>
+    <div className="min-h-screen bg-neutral-900">
+      <Header />
+      <CollectionInfo />
+      <SalesCard />
+    </div>
   )
 }
 
