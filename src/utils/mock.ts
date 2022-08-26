2c4d4e49daebc424f85540351879a48798ad5ba1
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-26 19:31:02 -0300

    feat: add mock util

---
+++ src/utils/mock.ts
@@ -0,0 +1,18 @@
+export const data = {
+  collectionName: 'Bored Ape Yacht Club',
+  token: '0x7b123f53421b1bF8533339BFBdc7C98aA94163db',
+  lifetime: '100d',
+  items: '10k',
+  royalties: '7.5%',
+  floor: '145',
+  vol24h: {
+    total: '1.2M',
+    performance: '12.02%'
+  },
+  vol7d: {
+    total: '12.9K',
+    performance: '1.29%'
+  },
+  sales: 2300,
+  hypeTrend: 'Very Hyped'
+}
