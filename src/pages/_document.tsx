fbb64220120ca11b2def197c3e34f20739e1b3cf
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-26 19:28:17 -0300

    feat(_document): add `Manrope` font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,43 +1,25 @@
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript
-} from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()]
-      }
-    } finally {
-      sheet.seal()
-    }
-  }
-
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=optional"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;700&display=swap"
             rel="stylesheet"
+          />
+          {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+          <title>LUMX NFT - COLLECTION</title>
+          <meta
+            name="description"
+            content="Lemonade helps you to create a playlist based on the setlist of your favorite artist's shows."
           />
         </Head>
         <body>
