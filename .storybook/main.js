1735e7f6169b148616b0038584a9343d886c0f19
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-27 12:31:50 -0300

    chore: config storybook ts paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,21 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react",
-  "core": {
+  framework: "@storybook/react",
+  core: {
     "builder": "@storybook/builder-webpack5"
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
   }
-}
+};
