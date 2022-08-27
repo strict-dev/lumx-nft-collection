23e33f29fea2ec538ead4c03a6657a2f76bc372c
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-27 17:17:50 -0300

    refactor(index): remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
-import { fetchedData } from 'atoms/fetchedData'
 import CollectionInfo from 'components/CollectionInfo'
 import Header from 'components/Header'
 import SalesCard from 'components/SalesCard'
 import type { NextPage } from 'next'
-import { useRecoilState } from 'recoil'
-import { data } from 'utils/mock'
 
 const Home: NextPage = () => {
-  const [, setData] = useRecoilState(fetchedData)
-  setData(data)
-
   return (
     <div className="min-h-screen bg-neutral-900">
       <Header />
