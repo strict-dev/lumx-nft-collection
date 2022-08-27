eebdc2306c048d2d86f02ae78fd5b3bad60ab757
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-27 17:21:45 -0300

    feat(CollectionInfo): add Loader and useData

--- src/components/CollectionInfo/collectionInfo.test.tsx
+++ src/components/CollectionInfo/collectionInfo.test.tsx
@@ -1,10 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render } from '@testing-library/react'
+import { RecoilRoot } from 'recoil'
 
 import CollectionInfo from '.'
 
 describe('<CollectionInfo />', () => {
   it('should render the heading', () => {
-    const { container } = render(<CollectionInfo />)
+    const queryClient = new QueryClient()
+
+    const { container } = render(
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <CollectionInfo />
+        </RecoilRoot>
+      </QueryClientProvider>
+    )
 
     expect(container.firstChild).toMatchSnapshot()
   })
--- src/components/CollectionInfo/index.tsx
+++ src/components/CollectionInfo/index.tsx
@@ -7,10 +7,18 @@ import TrendingUpIcon from '@public/assets/trending-up.svg'
 import CardWrapper from 'components/CardWrapper'
 import CollectionInfoGridItem from 'components/CollectionInfoGridItem'
 import Container from 'components/Container'
+import { useData } from 'hooks/useData'
 import Image from 'next/image'
-import { data } from 'utils/mock'
+
+import { Loader } from './Loader'
 
 const CollectionInfo = () => {
+  const { data, isLoading } = useData()
+
+  if (isLoading) {
+    return <Loader />
+  }
+
   return (
     <Container className="pt-20 ">
       <CardWrapper className="mb-14">
@@ -18,11 +26,11 @@ const CollectionInfo = () => {
           <div className="flex items-center justify-between gap-2">
             <Image src={UserPhoto} alt="User" width={32} height={32} />
             <div className="flex flex-col justify-between">
-              <h1 className="text-base font-bold">{data.collectionName}</h1>
+              <h1 className="text-base font-bold">{data?.collectionName}</h1>
 
               <div className="flex">
                 <p className="text-sm font-normal truncate max-w-[140px] text-neutral-600">
-                  {data.token}
+                  {data?.token}
                 </p>
                 <button>
                   <Image src={CopyIcon} alt="User" width={16} height={16} />
@@ -55,29 +63,33 @@ const CollectionInfo = () => {
 
         <hr className="border-neutral-700" />
 
-        <div className="grid grid-flow-row grid-cols-3 grid-rows-2 gap-2 pt-4">
-          <CollectionInfoGridItem title={'lifetime'} data={data.lifetime} />
+        <div className="grid items-baseline grid-flow-row grid-cols-3 grid-rows-2 gap-2 pt-4">
+          <CollectionInfoGridItem title={'lifetime'} data={data?.lifetime} />
 
-          <CollectionInfoGridItem title={'items'} data={data.items} />
+          <CollectionInfoGridItem title={'items'} data={data?.items} />
 
-          <CollectionInfoGridItem title={'floor'} data={data.floor} />
+          <CollectionInfoGridItem title={'floor'} data={data?.floor} />
 
-          <CollectionInfoGridItem title={'royalties'} data={data.royalties} />
+          <CollectionInfoGridItem title={'royalties'} data={data?.royalties} />
 
-          <CollectionInfoGridItem title={'vol24h'} data={data.floor} type="vol">
+          <CollectionInfoGridItem
+            title={'vol24h'}
+            data={data?.floor}
+            type="vol"
+          >
             <div className="flex items-center justify-center gap-1">
               <Image src={TrendingUpIcon} alt="Icon" width={16} height={16} />
               <span className="font-bold text-support-success">
-                {data.vol24h.performance}
+                {data?.vol24h.performance}
               </span>
             </div>
           </CollectionInfoGridItem>
 
-          <CollectionInfoGridItem title={'vol7d'} data={data.floor} type="vol">
+          <CollectionInfoGridItem title={'vol7d'} data={data?.floor} type="vol">
             <div className="flex items-center justify-center gap-1">
               <Image src={TrendingUpIcon} alt="Icon" width={16} height={16} />
               <span className="font-bold text-support-error">
-                {data.vol7d.performance}
+                {data?.vol7d.performance}
               </span>
             </div>
           </CollectionInfoGridItem>
