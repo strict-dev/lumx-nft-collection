61473b24662ef55976cf2d037ae1804a7ef23391
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-27 12:32:42 -0300

    feat(mock): add dataset generator mock

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 export const data = {
   collectionName: 'Bored Ape Yacht Club',
   token: '0x7b123f53421b1bF8533339BFBdc7C98aA94163db',
@@ -15,4 +17,54 @@ export const data = {
   },
   sales: 2300,
   hypeTrend: 'Very Hyped'
+}
+
+export const scatterDataset = () => {
+  const dataset = {
+    datasets: [
+      {
+        label: '',
+        data: Array.from({ length: 20 }, () => ({
+          x: faker.datatype.number({ min: 0, max: 100 }),
+          y: faker.datatype.number({ min: 0, max: 100 })
+        })),
+        backgroundColor: 'rgba(255, 99, 132, 1)'
+      },
+      {
+        label: '',
+        data: Array.from({ length: 20 }, () => ({
+          x: faker.datatype.number({ min: 0, max: 100 }),
+          y: faker.datatype.number({ min: 0, max: 100 })
+        })),
+        backgroundColor: '#26a7ac'
+      },
+      {
+        label: '',
+        data: Array.from({ length: 20 }, () => ({
+          x: faker.datatype.number({ min: 0, max: 100 }),
+          y: faker.datatype.number({ min: 0, max: 100 })
+        })),
+        backgroundColor: '#638dff'
+      }
+    ]
+  }
+
+  return dataset
+}
+
+export const barsDataset = () => {
+  const dataset = {
+    labels: Array.from({ length: 20 }, () =>
+      faker.datatype.number({ min: 0, max: 10 })
+    ),
+    datasets: [
+      {
+        data: Array.from({ length: 20 }, () =>
+          faker.datatype.number({ min: 0, max: 10 })
+        )
+      }
+    ]
+  }
+
+  return dataset
 }
