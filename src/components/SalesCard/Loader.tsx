3f9b8666e8a203ddad52213e6ea4a681bff46b81
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-28 21:51:59 -0300

    refactor: remove `Loader`

--- src/components/SalesCard/Loader.tsx
+++
@@ -1,8 +0,0 @@
-import CardWrapper from 'components/CardWrapper'
-import { GridLoader } from 'react-spinners'
-
-export const ChartLoader = () => (
-  <CardWrapper className="flex flex-col items-center justify-center h-64 gap-5 mb-2 bg-ne">
-    <GridLoader color="#6e7191" />
-  </CardWrapper>
-)
