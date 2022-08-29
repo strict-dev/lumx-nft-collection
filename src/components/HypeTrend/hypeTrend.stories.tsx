3c70d9a80b3efbe0ba9244193607b8d76e1ba629
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-28 21:57:21 -0300

    feat(hyperTrend): add `HyperTrend` component

---
+++ src/components/HypeTrend/hypeTrend.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, Story } from '@storybook/react'
+
+import HypeTrend from '.'
+
+export default {
+  title: 'HypeTrend',
+  component: HypeTrend
+} as Meta
+
+export const Default: Story = () => <HypeTrend />
---
+++ src/components/HypeTrend/hypeTrend.test.tsx
@@ -0,0 +1,8 @@
+import '@testing-library/react'
+
+describe('<HypeTrend />', () => {
+  it('should render the heading', () => {
+    // const { container } = render(<HypeTrend />)
+    // expect(container.firstChild).toMatchSnapshot()
+  })
+})
